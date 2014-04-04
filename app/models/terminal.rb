41b29f28cddc4dbe8cd8991a861e784c16ecc5fd
Yoshinori Ueda <uedayo@gmail.com>
2014-04-04 17:20:20 +0900

    add os references

--- app/models/terminal.rb
+++ app/models/terminal.rb
@@ -1,6 +1,6 @@
 class Terminal < ActiveRecord::Base
   belongs_to :terminal_master
   belongs_to :color
-  belongs_to :updated_os
+  belongs_to :updated_os, class_name: 'OperatingSystem', foreign_key: 'updated_os_id', validate: true
   belongs_to :place
 end
--- app/models/terminal_master.rb
+++ app/models/terminal_master.rb
@@ -3,7 +3,7 @@ class TerminalMaster < ActiveRecord::Base
   belongs_to :maker
   belongs_to :series
   belongs_to :model
-  belongs_to :default_os
+  belongs_to :default_os, class_name: 'OperatingSystem', foreign_key: 'default_os_id', validate: true
   belongs_to :screen_inch
   belongs_to :screen_width
   belongs_to :screen_height
