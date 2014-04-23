6d03a0fc6e78432b008203d4dd5d9d96a0bca511
Yoshinori Ueda <uedayo@gmail.com>
2014-04-23 15:12:19 +0900

    modify unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -28,7 +28,6 @@ working_directory "/home/rbenv/phone-rental" # available in 0.94.0+
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
 #listen "/path/to/.unicorn.sock", :backlog => 64
-listen "/tmp/nginx.sock", :backlog => 64
 listen 3001, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
@@ -36,7 +35,7 @@ timeout 30
 
 # feel free to point this anywhere accessible on the filesystem
 #pid "/path/to/app/shared/pids/unicorn.pid"
-pid "/home/rbenv/phone-rental/pids/unicorn.pid"
+pid "/home/rbenv/phone-rental/tmp/pids/unicorn.pid"
 
 # By default, the Unicorn logger will write to stderr.
 # Additionally, ome applications/frameworks log to stderr or stdout,
